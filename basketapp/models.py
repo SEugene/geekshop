470315a1c394c83a867592d0b6f57b4c93197a5d
SEugene <sea.05@list.ru>
2021-11-14 18:23:55 +0300

    Lesson 5. Basket quantity and sum added

--- basketapp/models.py
+++ basketapp/models.py
@@ -9,3 +9,15 @@ class Basket(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(verbose_name="количество", default=0)
     add_datetime = models.DateTimeField(verbose_name="время добавления", auto_now_add=True)
+
+    def totalsum(self):
+        total_sum = 0
+        for item in self:
+            total_sum += item.product.price * item.quantity
+        return total_sum
+
+    def totalqnt(self):
+        total_qnt = 0
+        for item in self:
+            total_qnt += item.quantity
+        return total_qnt
